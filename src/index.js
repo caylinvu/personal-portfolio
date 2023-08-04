a8969f38dd6014060c944f2d46551d1bd0b9f679
Caylin Vu <caylinvu@gmail.com>
2023-08-04 17:17:02 -0500

    Add links to add project function

--- src/index.js
+++ src/index.js
@@ -1,23 +1,30 @@
 const myProjects = document.querySelector('.my-projects');
 
-const addProject = (name, imgLoc, description) => {
+const addProject = (name, imgLoc, repoLinkLoc, liveLinkLoc, description) => {
     const projectDiv = document.createElement('div');
 
     const img = document.createElement('img');
     img.src = imgLoc;
+    img.className = 'project-img';
 
     const projectInfo = document.createElement('div');
 
     const projectName = document.createElement('h3');
     projectName.textContent = name;
 
+    const repoLink = document.createElement('a');
+    repoLink.href = repoLinkLoc;
+
+    const liveLink = document.createElement('a');
+    liveLink.href = liveLinkLoc;
+
     const ghIcon = document.createElement('img');
-    ghIcon.src = '../dist/images/github.svg';
+    ghIcon.src = 'images/github.svg';
     ghIcon.className = 'icon';
 
-    const liIcon = document.createElement('img');
-    liIcon.src = '../dist/images/linkedin.svg';
-    liIcon.className = 'icon';
+    const externalIcon = document.createElement('img');
+    externalIcon.src = 'images/open-in-new.svg';
+    externalIcon.className = 'icon';
 
     const projectDescription = document.createElement('p');
     projectDescription.textContent = description;
@@ -26,10 +33,14 @@ const addProject = (name, imgLoc, description) => {
     projectDiv.appendChild(img);
     projectDiv.appendChild(projectInfo);
     projectInfo.appendChild(projectName);
-    projectInfo.appendChild(ghIcon);
-    projectInfo.appendChild(liIcon);
+    projectInfo.appendChild(repoLink);
+    projectInfo.appendChild(liveLink);
+    repoLink.appendChild(ghIcon);
+    liveLink.appendChild(externalIcon);
     projectDiv.appendChild(projectDescription);
 }
+
+addProject('Battleship', 'images/battleship.png', 'https://github.com/caylinvu/battleship', 'https://caylinvu.github.io/battleship/', 'Battleship game created using Javascript. User is able to place ships on the board and play against an AI.');
 
 // TO DO
 
