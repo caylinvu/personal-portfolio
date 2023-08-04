e959f4bd845dff52d44fff98cca5564509fda815
Caylin Vu <caylinvu@gmail.com>
2023-08-04 16:15:57 -0500

    Create add project function

--- src/index.js
+++ src/index.js
@@ -1,14 +1,41 @@
+const myProjects = document.querySelector('.my-projects');
 
+const addProject = (name, imgLoc, description) => {
+    const projectDiv = document.createElement('div');
+
+    const img = document.createElement('img');
+    img.src = imgLoc;
+
+    const projectInfo = document.createElement('div');
+
+    const projectName = document.createElement('h3');
+    projectName.textContent = name;
+
+    const ghIcon = document.createElement('img');
+    ghIcon.src = '../dist/images/github.svg';
+    ghIcon.className = 'icon';
+
+    const liIcon = document.createElement('img');
+    liIcon.src = '../dist/images/linkedin.svg';
+    liIcon.className = 'icon';
+
+    const projectDescription = document.createElement('p');
+    projectDescription.textContent = description;
+
+    myProjects.appendChild(projectDiv);
+    projectDiv.appendChild(img);
+    projectDiv.appendChild(projectInfo);
+    projectInfo.appendChild(projectName);
+    projectInfo.appendChild(ghIcon);
+    projectInfo.appendChild(liIcon);
+    projectDiv.appendChild(projectDescription);
+}
 
 // TO DO
 
 // CREATE MY PROJECTS
 
-// CREATE CONTACT ME
-
 // FILL IN PROJECT INFO
-
-// FILL IN CONTACT INFO
 
 // STYLE ABOUT ME
 
